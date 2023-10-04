89892b0677bbb36f7ea77b209d0915930af0d034
Vivek Patel <vivekptl9@gmail.com>
2023-10-04 23:24:58 +0200

    Meaning.py with Ternary op

--- Python_Practice/meaning.py
+++ Python_Practice/meaning.py
@@ -1,6 +1,9 @@
 meaning = 8
-print(" ")
-if meaning >10:
-    print("Right on!")
-else:
-    print ("Not Today")
+# print(" ")
+# if meaning >10:
+#     print("Right on!")
+# else:
+#     print ("Not Today")
+
+#? Ternary Operator Example
+print ("Right On")if meaning >10 else print("Not Today")
