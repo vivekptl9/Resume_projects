6cbea02e51bfb95ba5dafd8e368865a5aac0bd73
Vivek Patel <vivekptl9@gmail.com>
2023-10-04 22:53:07 +0200

    Welcome message file

---
+++ Python_Practice/welcome.py
@@ -0,0 +1,12 @@
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+        line
+#line04 = "********************"
+#line05 = "********************"
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
