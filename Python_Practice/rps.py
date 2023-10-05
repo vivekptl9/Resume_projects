8a57106d0b36746868d833b11522b6ee38c7e70c
Vivek Patel <vivekptl9@gmail.com>
2023-10-05 13:24:25 +0200

    Added emojo based conditions for the game

--- Python_Practice/rps.py
+++ Python_Practice/rps.py
@@ -4,17 +4,34 @@
 #?
 # TODO: 
  # * ------------------------------------------------------------------------ * #
+# TODO: A basic program to make a Rock, Paper Scissor Game using python
+
+#? Importing the libraries required for this program
 import sys
 import random
- 
+
 print("")
+#? Variable defining the choice made by the player
 playerchoice = input("Enter ... \n1 for Rock, \n2 for Paper, \n3 for Scissors: \n\n ")
 player = int(playerchoice)
 if player < 1 | player > 3:
     sys.exit("You must enter 1,2,or 3.")
+#? Variable defining the choice made by the computer
 computerchoice = random.choice("123")
 computer = int(computerchoice)
+#-------------------------------------------------------------------------------
 print(" ")
 print("You chose " + playerchoice + ".")
 print("Python chose " + computerchoice + ".")
 print(" ")
+
+if player == 1 and computer ==3:
+    print("🙌 You Win..🎉🎊")
+elif player == 2 and computer ==1:
+    print("🙌 You Win..🎉🎊")
+elif player == 3 and computer ==2:
+    print("🙌 You Win..🎉🎊")
+elif player == computer:
+    print("😒 It's a Tie Game..!")
+else:
+    print("🐍 Python Wins..🎉🎊")
