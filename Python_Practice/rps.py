4217de376387f4887cd572ea9ff6a1ec1e34cb17
Vivek Patel <vivekptl9@gmail.com>
2023-10-06 14:23:51 +0200

    modified the game for repition

--- Python_Practice/rps.py
+++ Python_Practice/rps.py
@@ -15,34 +15,44 @@ class RPS(Enum):
     ROCK = 1
     PAPER = 2
     SCISSORS = 3
-print (RPS(2)) #! Different ways to call class objects
-print (RPS.ROCK)
-print (RPS["ROCK"])
-print (RPS.ROCK.value)
-# sys.exit()
 
-print("")
-#? Variable defining the choice made by the player
-playerchoice = input("Enter ... \n1 for Rock, \n2 for Paper, \n3 for Scissors: \n\n ")
-player = int(playerchoice)
-if player < 1 or player > 3:
-    sys.exit("You must enter 1,2,or 3.")
-#? Variable defining the choice made by the computer
-computerchoice = random.choice("123")
-computer = int(computerchoice)
-#-------------------------------------------------------------------------------
-print(" ")
-print("You chose " + playerchoice + ".")
-print("Python chose " + computerchoice + ".")
-print(" ")
+playagain = True
 
-if player == 1 and computer ==3:
-    print("🙌 You Win..🎉🎊")
-elif player == 2 and computer ==1:
-    print("🙌 You Win..🎉🎊")
-elif player == 3 and computer ==2:
-    print("🙌 You Win..🎉🎊")
-elif player == computer:
-    print("😒 It's a Tie Game..!")
-else:
-    print("🐍 Python Wins..🎉🎊")
+while playagain:
+    
+    print (RPS(2)) #! Different ways to call class objects
+    print (RPS.ROCK)
+    print (RPS["ROCK"])
+    print (RPS.ROCK.value)
+    # sys.exit()
+
+    print("")
+    #? Variable defining the choice made by the player
+    playerchoice = input("Enter ... \n1 for Rock, \n2 for Paper, \n3 for Scissors: \n\n ")
+    player = int(playerchoice)
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1,2,or 3.")
+    #? Variable defining the choice made by the computer
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
+    #-------------------------------------------------------------------------------
+    print("\nYou chose " + playerchoice + ".")
+    print("Python chose " + computerchoice + ".\n")
+
+    if player == 1 and computer ==3:
+        print("🙌 You Win..🎉🎊")
+    elif player == 2 and computer ==1:
+        print("🙌 You Win..🎉🎊")
+    elif player == 3 and computer ==2:
+        print("🙌 You Win..🎉🎊")
+    elif player == computer:
+        print("😒 It's a Tie Game..!")
+    else:
+        print("🐍 Python Wins..🎉🎊")
+    playagain = input("\n Play again? \n Y for Yes or \n Q to quit\n\n")
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("\n Thank you for playing!")
+        playagain = False
+sys.exit("Bye  .! 🙋‍♂️")
