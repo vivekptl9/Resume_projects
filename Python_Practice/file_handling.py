c0d070181550fcef2ceeef4dd8ada663676635a0
Vivek Patel <vivekptl9@gmail.com>
2023-10-15 15:46:20 +0200

    File handling in python with OS

---
+++ Python_Practice/file_handling.py
@@ -0,0 +1,14 @@
+import os
+path = "D:\\Resume_projects\\test.txt"
+path2 ="D:\\Resume_projects\\Python_Practice" 
+
+if os.path.exists(path):
+    print("the path exists")
+    if os.path.isfile(path):
+        print("File exists")
+        print(path)
+if os.path.isdir(path2):
+    print("Directory exists")
+    print(path2)
+else:
+    print("the path Do not exist")
