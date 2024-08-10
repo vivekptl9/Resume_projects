5d3002dbc170327edd3faea9bd94050e9b47d2dc
Vivek Patel <vivekptl9@gmail.com>
2024-08-10 15:20:55 +0200

    Completed the project

--- Python_Practice/Python_GUI/Data_entry_GUI.py
+++ Python_Practice/Python_GUI/Data_entry_GUI.py
@@ -29,6 +29,34 @@ def load_data():
 
     for value_tuple in list_values[1:]:
         treeview.insert('', tk.END, values=value_tuple)
+        
+
+def insert_row():
+    name = name_entry.get()
+    age = int(age_spinbox.get())
+    subscription_status = status_combobox.get()
+    employment_status = "Employed" if a.get() else "Unemployed"
+
+    # print(name, age, subscription_status, employment_status)
+
+    # Insert row into Excel sheet
+    path = r"d:\Resume_projects\Python_Practice\Python_GUI\people.xlsx"
+    workbook = openpyxl.load_workbook(path)
+    sheet = workbook.active
+    row_values = [name, age, subscription_status, employment_status]
+    sheet.append(row_values)
+    workbook.save(path)
+
+    # Insert row into treeview
+    treeview.insert('', tk.END, values=row_values)
+
+    # Clear the values
+    name_entry.delete(0, "end")
+    name_entry.insert(0, "Name")
+    age_spinbox.delete(0, "end")
+    age_spinbox.insert(0, "Age")
+    status_combobox.set(combo_list[0])
+    checkbutton.state(["!selected"])
     
 # Load the .tcl file as a script
 try:
@@ -73,7 +101,7 @@ a=tk.BooleanVar()
 checkbutton = ttk.Checkbutton(widgets_frame,text="Employed", variable=a)
 checkbutton.grid(row=3, column=0,  padx=5, pady=5, sticky="nsew")
 
-button = ttk.Button(widgets_frame,text="Insert")
+button = ttk.Button(widgets_frame,text="Insert",command=insert_row)
 button.grid(row=4, column=0, padx=5, pady=5, sticky="ew")
 
 seperator = ttk.Separator(widgets_frame)
