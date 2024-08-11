5d129420ba250eb5f358bd9257a70c19ca338558
Vivek Patel <vivekptl9@gmail.com>
2024-08-11 16:00:21 +0200

    new project of using python to generate Chat GPT response

---
+++ Python_Practice/Python_GUI/Py-to-GPT/main.py
@@ -0,0 +1,72 @@
+import os
+# import openai
+import customtkinter as ctk
+
+root = ctk.CTk()
+root.geometry("750x750")
+
+root.title("ChatGPT Project Idea Generator")
+ctk.set_appearance_mode("dark")
+
+
+title_label = ctk.CTkLabel(
+    root, text="Project Idea Generator", font=ctk.CTkFont(size=30, weight="bold"))
+title_label.pack(padx=10, pady=(40, 20))
+
+
+frame = ctk.CTkFrame(root)
+frame.pack(fill="x", padx=100)
+
+language_frame = ctk.CTkFrame(frame)
+language_frame.pack(padx=100, pady=(20, 5), fill="both")
+language_label = ctk.CTkLabel(
+    language_frame, text="Programming Language", font=ctk.CTkFont(weight="bold"))
+language_label.pack()
+language_dropdown = ctk.CTkComboBox(
+    language_frame, values=["Python", "Java", "C++", "JavaScript", "Golang"])
+language_dropdown.pack(pady=10)
+
+difficulty_frame = ctk.CTkFrame(frame)
+difficulty_frame.pack(padx=100, pady=5, fill="both")
+difficulty_label = ctk.CTkLabel(
+    difficulty_frame, text="Project Difficulty", font=ctk.CTkFont(weight="bold"))
+difficulty_label.pack()
+difficulty_value = ctk.StringVar(value="Easy")
+radiobutton1 = ctk.CTkRadioButton(
+    difficulty_frame, text="Easy", variable=difficulty_value, value="Easy")
+radiobutton1.pack(side="left", padx=(20, 10), pady=10)
+radiobutton2 = ctk.CTkRadioButton(
+    difficulty_frame, text="Medium", variable=difficulty_value, value="Medium")
+radiobutton2.pack(side="left", padx=(20, 10), pady=10)
+radiobutton3 = ctk.CTkRadioButton(
+    difficulty_frame, text="Hard", variable=difficulty_value, value="Hard")
+radiobutton3.pack(side="left", padx=(20, 10), pady=10)
+
+features_frame = ctk.CTkFrame(frame)
+features_frame.pack(padx=100,pady=5,fill="both")
+features_label=ctk.CTkLabel(features_frame,text="Features",font=ctk.CTkFont(weight="bold"))
+features_label.pack()
+
+checkbox1=ctk.CTkCheckBox(features_frame,text="Database")
+checkbox1.pack(side="left",padx=50,pady=10)
+checkbox2= ctk.CTkCheckBox(features_frame, text="API")
+checkbox2.pack(side="left", padx=50, pady=10)
+
+
+root.mainloop()
+
+# # Manually get the environment variable
+# api_key = os.getenv("OPENAI_API_KEY")
+
+# if api_key:
+#     print("API key is set.")
+#     openai.api_key = api_key
+# else:
+#     print("API key not found in environment variables.")
+
+# response = openai.ChatCompletion.create(
+#     model="gpt-3.5-turbo",
+#     messages=[{"role": "user", "content": "Hi Chat GPT. Say hi back"}]
+# )
+
+# print(response)
