205c1ffa5b678cdad8c9e29db8291175723d5721
Vivek Patel <vivekptl9@gmail.com>
2024-08-11 14:34:39 +0200

    arragning projects

--- Python_Practice/Python_GUI/Py-to-xl/Data_entry_GUI.py
+++ Python_Practice/Python_GUI/Py-to-xl/Data_entry_GUI.py
@@ -4,19 +4,20 @@ import openpyxl
 
 # Create the root window
 root = tk.Tk()
-style =ttk.Style(root)
+style = ttk.Style(root)
 # Correct full path to the .tcl file
 theme_path_d = r"d:\Resume_projects\Python_Practice\Python_GUI\forest-dark.tcl"
 theme_path_l = r"d:\Resume_projects\Python_Practice\Python_GUI\forest-light.tcl"
 combo_list = ["Subscribed", "Not Subscribed", "Other"]
+
 
 def toggle_mode():
     if mode_switch.instate(["selected"]):
         style.theme_use("forest-light")
     else:
         style.theme_use("forest-dark")
-        
-        
+
+
 def load_data():
     path = r"d:\Resume_projects\Python_Practice\Python_GUI\people.xlsx"
     workbook = openpyxl.load_workbook(path)
@@ -29,7 +30,7 @@ def load_data():
 
     for value_tuple in list_values[1:]:
         treeview.insert('', tk.END, values=value_tuple)
-        
+
 
 def insert_row():
     name = name_entry.get()
@@ -57,17 +58,18 @@ def insert_row():
     age_spinbox.insert(0, "Age")
     status_combobox.set(combo_list[0])
     checkbutton.state(["!selected"])
-    
+
+
 # Load the .tcl file as a script
 try:
     root.tk.call("source", theme_path_d)  # Source the .tcl file
     style.theme_use("forest-dark")  # Apply the theme
 except tk.TclError as e:
     print(f"Error: {e}")
-    
+
 try:
     root.tk.call("source", theme_path_l)  # Source the .tcl file
-   # style.theme_use("forest-light")  # Apply the theme
+# style.theme_use("forest-light")  # Apply the theme
 except tk.TclError as e:
     print(f"Error: {e}")
 
@@ -80,36 +82,36 @@ except tk.TclError as e:
 frame = ttk.Frame(root)
 frame.pack()
 
-widgets_frame = ttk.LabelFrame(frame,text="Insert Row")
-widgets_frame.grid(row=0,column=0,padx=20,pady=10)
+widgets_frame = ttk.LabelFrame(frame, text="Insert Row")
+widgets_frame.grid(row=0, column=0, padx=20, pady=10)
 
 name_entry = ttk.Entry(widgets_frame)
-name_entry.insert(0,"Name")
-name_entry.bind("<FocusIn>", lambda e: name_entry.delete("0","end"))
-name_entry.grid(row=0,column=0,padx=5, pady=(0,5),sticky="ew")
+name_entry.insert(0, "Name")
+name_entry.bind("<FocusIn>", lambda e: name_entry.delete("0", "end"))
+name_entry.grid(row=0, column=0, padx=5, pady=(0, 5), sticky="ew")
 
 
-age_spinbox = ttk.Spinbox(widgets_frame, from_=18, to= 100)
-age_spinbox.grid(row=1, column=0, padx=5, pady=5,sticky="ew")
+age_spinbox = ttk.Spinbox(widgets_frame, from_=18, to=100)
+age_spinbox.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
 age_spinbox.insert(0, "Age")
 
 status_combobox = ttk.Combobox(widgets_frame, values=combo_list)
 status_combobox.current(0)
 status_combobox.grid(row=2, column=0,  padx=5, pady=5, sticky="ew")
 
-a=tk.BooleanVar()
-checkbutton = ttk.Checkbutton(widgets_frame,text="Employed", variable=a)
+a = tk.BooleanVar()
+checkbutton = ttk.Checkbutton(widgets_frame, text="Employed", variable=a)
 checkbutton.grid(row=3, column=0,  padx=5, pady=5, sticky="nsew")
 
-button = ttk.Button(widgets_frame,text="Insert",command=insert_row)
+button = ttk.Button(widgets_frame, text="Insert", command=insert_row)
 button.grid(row=4, column=0, padx=5, pady=5, sticky="ew")
 
 seperator = ttk.Separator(widgets_frame)
-seperator.grid(row=5,column=0, padx=(20,10),pady=10,sticky="ew")
+seperator.grid(row=5, column=0, padx=(20, 10), pady=10, sticky="ew")
 
 mode_switch = ttk.Checkbutton(
     widgets_frame, text="Mode", style="Switch", command=toggle_mode)
-mode_switch.grid(row=6,column=0,padx=5,pady=10,sticky="nsew")
+mode_switch.grid(row=6, column=0, padx=5, pady=10, sticky="nsew")
 
 
 treeFrame = ttk.Frame(frame)
