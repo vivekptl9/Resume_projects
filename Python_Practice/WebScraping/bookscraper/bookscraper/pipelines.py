045122436afacd82a573d6c942ebf36589ea8c9f
Vivek Patel <vivekptl9@gmail.com>
2024-08-21 20:09:30 +0200

    added different functions to clean the scarpped data before storing it in a file

--- Python_Practice/WebScraping/bookscraper/bookscraper/pipelines.py
+++ Python_Practice/WebScraping/bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,55 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+    #TODO Removes White spaces----------------------------------------------------------------------
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != "description":
+                value = adapter.get(field_name)
+                adapter[field_name] = value[0].strip()
+    
+    # TODO Converts to lower case ----------------------------------------------------------------------
+        lowercase_keys = ["category","product_type"]
+        for lowercase_key in lowercase_keys:
+            value =adapter.get(lowercase_key)
+            adapter[lowercase_key] =value.lower()
+    
+    # TODO converters prices to float values and removes currency sign----------------------------------------------------------------------
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+    
+    # TODO separates the availabitiy numbers----------------------------------------------------------------------
+        availability_string = adapter.get('availability')
+        split_string_array = availability_string.split('(')
+        if len(split_string_array) < 2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+    
+    # TODO Reviews convert from string to number--------------------------------------------------------------------
+        num_reviews_string = adapter.get('num_reviews')
+        adapter['num_reviews'] = int(num_reviews_string)
+        
+        stars_string = adapter.get('stars')
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == "zero":
+            adapter['stars'] = 0
+        elif stars_text_value == "one":
+            adapter['stars'] = 1
+        elif stars_text_value == "two":
+            adapter['stars'] = 2
+        elif stars_text_value == "three":
+            adapter['stars'] = 3
+        elif stars_text_value == "four":
+            adapter['stars'] = 4
+        elif stars_text_value == "five":
+            adapter['stars'] = 5
+
+        
         return item
