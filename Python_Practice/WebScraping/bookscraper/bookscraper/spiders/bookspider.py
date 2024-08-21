c132c89bb383515f665731f451e9d8fdb29423f5
Vivek Patel <vivekptl9@gmail.com>
2024-08-21 15:26:30 +0200

    Scrapy crawl to get all 1000 books

--- Python_Practice/WebScraping/bookscraper/bookscraper/spiders/bookspider.py
+++ Python_Practice/WebScraping/bookscraper/bookscraper/spiders/bookspider.py
@@ -18,5 +18,9 @@ class BookspiderSpider(scrapy.Spider):
             
         next_page = response.css("li.next a::attr(href)").get()
         if next_page is not None:
-            next_page_url = "https://books.toscrape.com" + next_page
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
             yield response.follow(next_page_url,callback = self.parse)
+
