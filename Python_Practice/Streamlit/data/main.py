0f19dff1e63653a948eb4ce5bc80c5ea699438ba
Vivek Patel <vivekptl9@gmail.com>
2024-09-04 19:15:38 +0200

    Log reg model for breast cancer prediction

--- Python_Practice/Streamlit/data/main.py
+++ Python_Practice/Streamlit/data/main.py
@@ -43,7 +43,7 @@ if selected_file:
     with col2:
         x_axis = st.selectbox("Select the X-axis", options=columns+["None"], index=None)
         y_axis = st.selectbox("Select the Y-axis", options=columns+["None"], index=None)
-        plot_list = ["Line Plot", "Bar Chart", "Scater Plot", "Distribution Plot", "Count Plot"]
+        plot_list = ["Line Plot", "Bar Chart", "Scatter Plot", "Distribution Plot", "Count Plot"]
         
         selected_plot = st.selectbox("Selected a Plot", options=plot_list, index=None)
         
@@ -51,6 +51,35 @@ if selected_file:
         st.write(y_axis)
         st.write(selected_plot)
         
+
+    if st.button("Generate Plots"):
+        fig, ax = plt.subplots(figsize =(6,4))
+        
+        if selected_plot == "Line Plot":
+            sns.lineplot(x=df[x_axis],y=df[y_axis],ax=ax)
+        
+        elif selected_plot == "Bar Chart":
+            sns.barplot(x=df[x_axis],y=df[y_axis],ax=ax)
+        
+        elif selected_plot == "Scatter Plot":
+            sns.scatterplot(x=df[x_axis], y=df[y_axis], ax=ax)
+        
+        elif selected_plot == "Distribution Plot":
+            sns.histplot(x=df[x_axis],kde=True,ax=ax)
+        
+        elif selected_plot == "Count Plot":
+            sns.countplot(x=df[x_axis],ax=ax)
+            
+            
+        ax.tick_params(axis="x", labelsize=10)
+        ax.tick_params(axis="y", labelsize=10)
+        
+        
+        plt.title(f"{selected_plot} of {y_axis} vs {x_axis}", fontsize=12)
+        plt.xlabel(x_axis,fontsize = 10)
+        plt.ylabel(y_axis,fontsize = 10)
+        st.pyplot(fig)
+            
 
     
 
