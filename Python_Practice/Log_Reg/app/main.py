7e3664b8b741045f4f9b6662fc4030f6b1ee9ea5
Vivek Patel <vivekptl9@gmail.com>
2024-09-09 23:36:13 +0200

    Log reg streamlit app

--- Python_Practice/Log_Reg/app/main.py
+++ Python_Practice/Log_Reg/app/main.py
@@ -8,7 +8,8 @@ import numpy as np
 
 
 def get_clean_data():
-    data = pd.read_csv("data.csv")
+    data = pd.read_csv(
+        "/d/Resume_projects/Python_Practice/Log_Reg/app/data.csv")
 
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
 
